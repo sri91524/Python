da33fda8bcebb12e38f526f16ba33f998dd8293b
sri91524 <sreeravish@gmail.com>
2025-05-07 11:56:07 -0400

    practice on literals-operators

---
+++ Example-Literals.py
@@ -0,0 +1,23 @@
+print(111_1111)
+# Octal
+print(0o123)
+# Hexadecimal
+print(0x123)
+# Float
+print(.4)
+print(4.)
+print(0.000000000001)
+#string - escape
+print("I 'm Monty Python")
+print('I \'m Monty Python')
+
+# Boolean
+print(True > False)
+print(False > True)
+
+#Expected Output
+#"I'm"
+#""learning""
+#"""Python"""
+
+print('"I\'m" \n ""learning"" \n """Python"""')
---
+++ Example-Operator.py
@@ -0,0 +1,49 @@
+#Exponents
+print("========Exponents=========")
+print(2 ** 3)
+print(2 ** 3.)
+print(2. ** 3)
+print(2. ** 3.)
+
+#Division
+print("========Division=========")
+print(6 / 3)
+print(6 / 3.)
+print(6. / 3)
+print(6. / 3.)
+
+#Integer Division
+print("========Integer Division=========")
+print(6 // 4)
+print(6. // 4)
+
+#rounded to lesser integer
+print(-6 // 4)
+print(6. // -4)
+
+#Modulo
+print("========Modulo=========")
+print(14 % 4)
+print(12 % 4.5)
+
+#Addition
+print("========Addition=========")
+print(-4 + 4)
+print(-4. + 8)
+
+#subtraction
+print("========Subtraction=========")
+print(-4 -4)
+print(4. -8)
+print(-1.1)
+
+print(+2)
+
+#operators
+print("========Operators=========")
+print(9 % 6 % 2)
+print(2 ** 2 ** 3) #right side binding
+print(-3 ** 2)
+print((-3) ** 2)
+print(-2 ** 3)
+print(-(3 ** 2))
